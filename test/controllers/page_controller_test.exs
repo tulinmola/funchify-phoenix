4bfc81b300587db4d7cd548d136e478cad8fac3c
tulinmola <tulinmola@gmail.com>
2017-03-01 12:17:38 +0100

    Fix home page test

--- test/controllers/page_controller_test.exs
+++ test/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule Funchify.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get conn, "/"
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    assert html_response(conn, 200) =~ "Welcome to the clicky-clicky world"
   end
 end
